e7ebd48b325efed5e5782fb7b1e834b41539abdd
Tima <ibatullintima@gmail.com>
2021-06-08 11:59:11 +0600

    hello dasd

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ const cay_hash = "3bbec084579cf27e0b80471f69128336";
 const api_cay = 'e53db8034e6247d000be41fd3d7cd4e1';
 const $search = document.querySelector('.search');
 const content_cards = document.querySelector('.content_cards');
-// const page = document.querySelector('.page');
-// const aroow_back = document.querySelector('.aroow_back');
-// const arrow_next = document.querySelector('.aroow_next');
-// let offsetc = 0;
-// let pagec = 1;
+const page = document.querySelector('.page');
+const aroow_back = document.querySelector('.aroow_back');
+const arrow_next = document.querySelector('.aroow_next');
+let offsetc = 0;
+let pagec = 1;
 const LIMIT = 21;
-// let total_heroes = 1493;
-// const mathTotal = Math.floor(total_heroes / LIMIT);
+let total_heroes = 1493;
+const mathTotal = Math.floor(total_heroes / LIMIT);
 
 const getRequest = (offset = 0, urlSearch = '', cb) => {
     const url = `http://gateway.marvel.com/v1/public/characters?&limit=${LIMIT}&offset=${offset}${!urlSearch ? null : `&nameStartsWith=${urlSearch}`}&orderBy=-modified&ts=1&apikey=${api_cay}&hash=${cay_hash}`; 
@@ -67,55 +67,54 @@ $search.addEventListener('input' , e => {
         )
     }
 })
-// // next-back
 
 
-// window.addEventListener('load' , () => {
-//     page.innerHTML = pagec;
-//     aroow_back.setAttribute('disabled' , true);
-// });
-// arrow_next.addEventListener('click' , e=> {
-//     e.preventDefault();
-//     aroow_back.removeAttribute('disabled');
+window.addEventListener('load' , () => {
+    page.innerHTML = pagec;
+    aroow_back.setAttribute('disabled' , true);
+});
+arrow_next.addEventListener('click' , e=> {
+    e.preventDefault();
+    aroow_back.removeAttribute('disabled');
 
-//     if(pagec >=1 && pagec <= mathTotal){
-//         if(pagec === mathTotal){
-//             getRequest('',`offset=${offsetc += LIMIT}&limit=${LIMIT}` ,res => {
-//                 pagec++;
-//                 page.innerHTML = pagec;
-//                 let temp = res.results.map(item => cardTemp(item)).join('');
-//                 content_cards.innerHTML = temp;
-//                 console.log(temp);
-//             });
-//         }else{
-//             getRequest('', `offset=${offsetc += LIMIT}&limit=${LIMIT}` ,res => {
-//                 pagec++;
-//                 page.innerHTML = pagec;
-//                 let temp = res.results.map(item => cardTemp(item)).join('');
-//                 content_cards.innerHTML = temp;
-//                 console.log(temp);
-//             })
-//         }
-//     }
-// });
+    if(pagec >=1 && pagec <= mathTotal){
+        if(pagec === mathTotal){
+            getRequest('',`offset=${offsetc += LIMIT}&limit=${LIMIT}` ,res => {
+                pagec++;
+                page.innerHTML = pagec;
+                let temp = res.results.map(item => cardTemp(item)).join('');
+                content_cards.innerHTML = temp;
+                console.log(temp);
+            });
+        }else{
+            getRequest('', `offset=${offsetc += LIMIT}&limit=${LIMIT}` ,res => {
+                pagec++;
+                page.innerHTML = pagec;
+                let temp = res.results.map(item => cardTemp(item)).join('');
+                content_cards.innerHTML = temp;
+                console.log(temp);
+            })
+        }
+    }
+});
 
-// aroow_back.addEventListener('click' , e => {
-//     e.preventDefault();
-//     if(pagec > 1){
-//         pagec --;
-//         if(pagec === 1){
-//             aroow_back.setAttribute('disabled' , true);
-//             offsetc = 0;
-//             page.innerHTML = pagec;
-//             getRequest('' ,`offset=${offsetc}&limit=${LIMIT}` , res => {
-//                 let temp = res.results.map(item => cardTemp(item)).join('');
-//                 content_cards.innerHTML = temp;
-//             })
-//         }else{
-//             getRequest('' ,`offset=${offsetc -= LIMIT}&limit=${LIMIT}`, res => {
-//                 let temp = res.results.map(item => cardTemp(item)).join('');
-//                 content_cards.innerHTML = temp;
-//             })
-//         }
-//     }
-// })
+aroow_back.addEventListener('click' , e => {
+    e.preventDefault();
+    if(pagec > 1){
+        pagec --;
+        if(pagec === 1){
+            aroow_back.setAttribute('disabled' , true);
+            offsetc = 0;
+            page.innerHTML = pagec;
+            getRequest('' ,`offset=${offsetc}&limit=${LIMIT}` , res => {
+                let temp = res.results.map(item => cardTemp(item)).join('');
+                content_cards.innerHTML = temp;
+            })
+        }else{
+            getRequest('' ,`offset=${offsetc -= LIMIT}&limit=${LIMIT}`, res => {
+                let temp = res.results.map(item => cardTemp(item)).join('');
+                content_cards.innerHTML = temp;
+            })
+        }
+    }
+})
